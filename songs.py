d1fb9f08ca1f10679e1d070facfd3c885696b882
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-04 22:15:18 +0300

    Добавил файл с дз по теме str

---
+++ songs.py
@@ -0,0 +1,6 @@
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
