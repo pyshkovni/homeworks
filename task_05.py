b6feef4408a0a6985deab52426fdd778c70bc605
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-11 16:45:37 +0300

    Добавил дз 2-7

---
+++ task_05.py
@@ -0,0 +1,16 @@
+
+import pprint
+
+my_favorite_songs = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+pprint.pprint(my_favorite_songs)
