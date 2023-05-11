0e2847d7c2ae43db4d3f73f6da5a9bab7b18552b
feaa79 <feaa79@gmail.com>
2023-05-11 21:20:36 +0300

    Переделано задание 1.1

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -7,10 +7,14 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, 
 # последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
-a1 = len('Waste a Moment, Staying\' Alive,')
-a2 = len('Waste a Moment, Staying\' Alive, A Sorta Fairytale,')
-a3 = len('Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up,')
-print(my_favorite_songs[:a1-1])
-print(my_favorite_songs[a3+1:])
-print(my_favorite_songs[a1+1:a2-1])
-print(my_favorite_songs[a2+1:a3-1])
+
+_a = ','
+_lst = []
+for _index, _char in enumerate(my_favorite_songs):
+    if _char == _a:
+        _lst.append(_index)
+
+print(my_favorite_songs[:_lst[0]])
+print(my_favorite_songs[_lst[3]+2:])
+print(my_favorite_songs[_lst[0]+2 : _lst[1]])
+print(my_favorite_songs[_lst[2]+2 : _lst[3]])
