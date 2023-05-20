f354fca09bcd2a502d0cfc94245e5c8d7d5eaef7
DENDENICH <128420482+DENDENICH@users.noreply.github.com>
2023-05-20 20:51:32 +0300

    task_1.1.py

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -4,5 +4,10 @@
 
 my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
 
+
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+print(f'первый трек {my_favorite_songs[0, 15]')
+print(f'последний трек {my_favorite_songs[-1, -14])
+print(f'второй трек {my_favorite_songs[17, 32])
+print(f'второй трек в конца {my_favorite_songs[-16, -28])
